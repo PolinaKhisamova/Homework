1c0fc8bd2483571afae63dfddbdc636f3e22a189
Polina <05no1192@mail.ru>
2023-06-17 16:57:26 +0800

    2 task done

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Enter the first number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the third number");
+int num3 = Convert.ToInt32(Console.ReadLine());
+if(num1>num2)
+{
+    if(num1>num3)
+    {
+        Console.WriteLine("max - " + num1);
+    }
+    else
+    {
+        Console.WriteLine("max - " + num3);
+    }
+}
+else
+{
+    if(num2>num3)
+    {
+        Console.WriteLine("max - " + num2);
+    }
+    else
+    {
+        Console.WriteLine("max - " + num3);
+    }
+}
