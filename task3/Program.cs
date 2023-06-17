b69218d0d44001ae0bbce1838b00ce79fcac1fab
Polina <05no1192@mail.ru>
2023-06-17 17:02:27 +0800

    3 task done

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter a number");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number%2 == 0)
+{
+    Console.WriteLine($"The number '{number}' is even");
+}
+else{
+    Console.WriteLine($"The number '{number}' is UNeven");
+}
